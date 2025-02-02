53e6425db7984350f3cd74206e2f579c8cbb7ebc
104-wonohfor <thang20973@gmail.com>
2025-02-02 23:16:23 +0700

    update

--- src/main/java/com/vdt/library_mangement/LibraryMangementApplication.java
+++ src/main/java/com/vdt/library_mangement/LibraryMangementApplication.java
@@ -3,9 +3,11 @@ package com.vdt.library_mangement;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+
 public class LibraryMangementApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/vdt/library_mangement/service/LibrarianService.java
+++ src/main/java/com/vdt/library_mangement/service/LibrarianService.java
@@ -1,4 +1,7 @@
 package com.vdt.library_mangement.service;
 
+import com.vdt.library_mangement.dto.LibrarianDto;
+
 public interface LibrarianService {
+    LibrarianDto createLibrarian(LibrarianDto librarianDto);
 }
